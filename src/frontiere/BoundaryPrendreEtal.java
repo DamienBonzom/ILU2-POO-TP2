e10043637dd5de2fc0656470e2b9f7ee7f247279
DamienBonzom <damienbonzom31@gmail.com>
2023-10-16 16:17:20 +0200

    apres le crash de paul sab

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,10 +13,47 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		boolean nomVendeurConnu = controlPrendreEtal.verifierIdentite(nomVendeur);
+		if (!nomVendeurConnu) {
+			StringBuilder vendeurInconnu = new StringBuilder();
+			vendeurInconnu.append(String.format(
+					"Je suis désolée %s mais il faut être un habitant de notre village pour commercer ici.\n",
+					nomVendeur));
+			System.out.println(vendeurInconnu.toString());
+		} else {
+			StringBuilder reponse = new StringBuilder();
+			reponse.append(
+					String.format("Bonjour %s, je vais regarder si je peux vous trouver un étal.\n", nomVendeur));
+			boolean etalDisponible = controlPrendreEtal.resteEtals();
+			if (!etalDisponible) {
+				StringBuilder etalNonDispo = new StringBuilder();
+				etalNonDispo.append(
+						String.format("Désolée %s je n'ai plus d'étal qui ne soit pas déjà occupé.\n", nomVendeur));
+				System.out.println(etalNonDispo.toString());
+			} else {
+				installerVendeur(nomVendeur);
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		StringBuilder questionProduit = new StringBuilder();
+		questionProduit.append("C'est parfait, il me reste un étal pour vous !\n");
+		questionProduit.append("Il me faudrait quelques renseignements :\n");
+		questionProduit.append("Quel produit souhaitez-vous cendre ?\n");
+		System.out.println(questionProduit.toString());
+		String produit = scan.nextLine();
+
+		StringBuilder questionQuantite = new StringBuilder();
+		questionQuantite.append("Combien souhaitez vous en vendre ?\n");
+		int nbProduit = Clavier.entrerEntier(questionQuantite.toString());
+
+		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+
+		if (numeroEtal != -1) {
+			StringBuilder installation = new StringBuilder();
+			installation.append(String.format("Le vendeur %s s'est installé à l'étal n°%d", nomVendeur, numeroEtal));
+		}
+
 	}
 }
--- src/histoire/TestControleursDesCas.java
+++ src/histoire/TestControleursDesCas.java
@@ -2,6 +2,7 @@ package histoire;
 
 import controleur.ControlAfficherVillage;
 import controleur.ControlEmmenager;
+import controleur.ControlPrendreEtal;
 import controleur.ControlVerifierIdentite;
 import personnages.Chef;
 import personnages.Gaulois;
@@ -44,12 +45,11 @@ public class TestControleursDesCas {
 
 		// Cas 4 : Prendre etal
 
-//		System.out.println("///////////////////////////////");
-//		System.out.println("Cas 4 : Prendre etal");
-//		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(
-//				controlVerifierIdentite, village);
-//		int numeroEtal = controlPrendreEtal.prendreEtal("Obelix", "fleur", 5);
-//		System.out.println("Obelix a pris l'étal n°" + numeroEtal);
+		System.out.println("///////////////////////////////");
+		System.out.println("Cas 4 : Prendre etal");
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite, village);
+		int numeroEtal = controlPrendreEtal.prendreEtal("Obelix", "fleur", 5);
+		System.out.println("Obelix a pris l'étal n°" + numeroEtal);
 
 		// Cas 5 : trouverEtalVendeur
 
